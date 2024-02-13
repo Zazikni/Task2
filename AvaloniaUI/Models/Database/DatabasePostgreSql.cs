cf58e4922bd720b4434ead9e0be5efcc8b1a24f8
Никита Чепельский <nikitachepelskii2@gmail.com>
2024-02-13 23:15:26 +0300

    + DatabasePostgreSql.AddUser + app.config params

--- AvaloniaUI/Models/Database/DatabasePostgreSql.cs
+++ AvaloniaUI/Models/Database/DatabasePostgreSql.cs
@@ -1,19 +1,85 @@
 ﻿using AvaloniaUI.Models.Users;
 using System;
+using Npgsql;
+using System.Threading.Tasks;
+using AvaloniaUI.Models.Security;
+using Microsoft.Data.Sqlite;
+using Serilog;
+using Tmds.DBus.Protocol;
 
 
 namespace AvaloniaUI.Models.Database
 {
-    internal class DatabasePostgreSql : IDatabase
+    internal class DatabasePostgreSql //: IDatabase
     {
-        public void AddUser(NewUser user)
+        #region fields
+        string _connectionString;
+        NpgsqlDataSource _dataSource;
+        #endregion
+        #region constructors
+        public DatabasePostgreSql(string host, string database, string username, string password, int port)
         {
-            throw new NotImplementedException();
+            NpgsqlConnectionStringBuilder _csb = new NpgsqlConnectionStringBuilder();
+            _csb.Host = host;
+            _csb.Database = database;
+            _csb.Username = username;
+            _csb.Password = password;
+            _csb.Port = port;
+            _connectionString = _csb.ToString();
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(_connectionString);
+            _dataSource = dataSourceBuilder.Build();
+
+            _init();
+
+
+        }
+        #endregion
+        #region methods
+        private async Task _init()
+        {
+            var x =  AddUser(new NewUser(name: "Nikita", login: "zazik", password: "K2342^%&KNKNKJw4cw3c4wr"));
+            var y = AddUser(new NewUser(name: "Oleg", login: "katsd", password: "712876asKasdsassdr"));
+            var z = AddUser(new NewUser(name: "Billy", login: "billy212", password: "45466&%#dt56asdasd."));
+            await x;
+            await y;
+            await z;
+        }
+        public async Task AddUser(NewUser user)
+        {
+            var connection = await _dataSource.OpenConnectionAsync();
+            try
+            {
+                NpgsqlCommand _command = new NpgsqlCommand();
+                _command.CommandText = "INSERT INTO Users (login, name, password) VALUES (@login, @name, @password)";
+                _command.Connection = connection;
+                _command.Parameters.AddWithValue("@login", user.Login);
+                _command.Parameters.AddWithValue("@name", user.Name);
+                _command.Parameters.AddWithValue("@password", PasswordHasher.CreateSHA256(user.Password));
+                await _command.ExecuteNonQueryAsync();
+
+
+            }
+            catch (NpgsqlException ex)
+            {
+                Log.Error($"Error wile adding new user to Users table name: {user.Name} password: {user.Password} login: {user.Login}\n {ex.Message}");
+
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error($"Error wile adding new user to Users table name: {user.Name}  password:  {user.Password} login: {user.Login}\n {ex.ToString()}");
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
         public User? GetUser(string login)
         {
             throw new NotImplementedException();
         }
+        #endregion
+
     }
 }
--- AvaloniaUI/ViewModels/AuthWindowViewModel.cs
+++ AvaloniaUI/ViewModels/AuthWindowViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using AvaloniaUI.Models.Database;
 using Microsoft.Data.Sqlite;
+using System.Configuration;
+using System;
 namespace AvaloniaUI.ViewModels
 {
     public class AuthWindowViewModel : ViewModelBase
     {
-        DatabaseSqlite database = new DatabaseSqlite();
+
+        DatabasePostgreSql database = new DatabasePostgreSql(
+            host: ConfigurationManager.AppSettings["PostgreHost"],  
+            database: ConfigurationManager.AppSettings["PostgreDatabase"],  
+            username: ConfigurationManager.AppSettings["PostgreUsername"],
+            password: ConfigurationManager.AppSettings["PostgrePassword"],
+            port: Convert.ToInt32(ConfigurationManager.AppSettings["PostgrePort"]));
 
 #pragma warning disable CA1822 // Mark members as static
         public string Greeting => "HELLO!";
