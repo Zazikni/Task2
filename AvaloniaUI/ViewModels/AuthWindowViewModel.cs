8723dca5a20353efe17c91e2aba7012827d2dd87
Nikita Chepelskii <nikitachepelskii2@gmail.com>
2024-02-14 17:14:39 +0300

    add ReactiveUi commands logic

--- AvaloniaUI/ViewModels/AuthWindowViewModel.cs
+++ AvaloniaUI/ViewModels/AuthWindowViewModel.cs
@@ -2,6 +2,15 @@
 using Microsoft.Data.Sqlite;
 using System.Configuration;
 using System;
+using ReactiveUI;
+using System.Windows.Input;
+using Avalonia.Controls;
+using Serilog;
+using AvaloniaUI.Models.Security;
+using System.Threading.Tasks;
+using Avalonia;
+using System.Reactive;
+using System.Diagnostics;
 namespace AvaloniaUI.ViewModels
 {
     public class AuthWindowViewModel : ViewModelBase
@@ -15,8 +24,51 @@ namespace AvaloniaUI.ViewModels
             port: Convert.ToInt32(ConfigurationManager.AppSettings["PostgrePort"]));
 
 #pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "HELLO!";
+        #region fields
+
+        private string _login = string.Empty;
+        private string _password = string.Empty;
+        public string Login {
+            get { return _login; }
+            set { this.RaiseAndSetIfChanged(ref _login, value); }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { this.RaiseAndSetIfChanged(ref _password, value); }
+        }
+
+        #endregion
+        #region commands
+        public ReactiveCommand<Unit, Unit> AuthUser { get; }
+        public ReactiveCommand<Unit, Unit> DoSomeThing { get; }
+        #endregion
+
+        public AuthWindowViewModel()
+        {
+            DoSomeThing = ReactiveCommand.Create(RunSomeThing);
+            AuthUser = ReactiveCommand.Create(AuthUserByLogPass);
+        }
+        #region commMethods
+        public async void RunSomeThing()
+        {
+            Log.Debug("RunSomeThing START");
+            await Task.Delay(10000);
+            Log.Debug("RunSomeThing END");
+
+        }
+        public async void AuthUserByLogPass()
+        {
+            Login = String.Empty;
+            Password = String.Empty;
+            Log.Debug($"Button command was clicked!");
+            Log.Debug($"TextBoxLogin: {Login} TextBoxPassword:{Password}");
+            await Task.Delay(10000);
+            bool access = await AuthenticationManager.AccessAllowed(login: Login, password: Password, database: database);
+
+        }
+        #endregion
 
 #pragma warning restore CA1822 // Mark members as static
     }
-}
+    }
