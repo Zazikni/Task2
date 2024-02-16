c6a7993a12a4dd6f959c11411606c90ff87f451d
Nikita Chepelskii <nikitachepelskii2@gmail.com>
2024-02-16 17:23:48 +0300

    + calс init

--- AvaloniaUI/ViewModels/MainWindowViewModel.cs
+++ AvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -1,37 +1,49 @@
-﻿using AvaloniaUI.Models.Database;
-using AvaloniaUI.Models.WindowManager;
+﻿using Events;
 using ReactiveUI;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AvaloniaUI.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private string _message;
-
+        #region fields
+        private string _message = "Wait a bit...";
         public string Message
         {
             get => _message;
             set => this.RaiseAndSetIfChanged(ref _message, value);
         }
+        #endregion
 
+        #region constructors
         public MainWindowViewModel()
         {
-            Message = "Hello from the new window!";
+            Log.Debug($"MainWindowViewModel init");
+
+            CalculateMessageAsync();
+
         }
-        #region fields
         #endregion
-        #region commands
-        #endregion
-        #region constructors
-        #endregion
-        #region commMethods
+
+        #region tasks
+        private async Task CalculateMessageAsync()
+        {
+            Log.Debug($"CalculateMessageAsync start");
+            string result = await Task.Run(() =>
+            {
+                Log.Debug($"Calculation start");
+                string tmp = EventsProgram.Run();
+                Log.Debug($"Calculation end");
+                return tmp;
+            });
+            Log.Debug($"CalculateMessageAsync finish");
+
+            Message = result;
+        }
 
         #endregion
+
     }
+
 }
