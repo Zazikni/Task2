b88bc43c0b928f64bdcbc64fa7856c05b13e16d7
Nikita Chepelskii <nikitachepelskii2@gmail.com>
2024-02-28 16:42:37 +0300

    changes in the project architecture

---
+++ Server/Models/Client/ClientObject.cs
@@ -0,0 +1,133 @@
+﻿using Models.Security;
+using Server.Models.Server;
+using System.Net.Sockets;
+
+namespace Server.Models.Client
+{
+    class ClientObject
+    {
+        private bool _isAuthorized;
+        public bool IsAuthorized
+        {
+            get
+            {
+                return _isAuthorized;
+            }
+        }
+        private bool _spamAllowed = false;
+        public bool SpamAllowed
+        {
+            get
+            {
+                return _spamAllowed;
+            }
+        }
+        protected internal string Id { get; } = Guid.NewGuid().ToString();
+        protected internal StreamWriter Writer { get; }
+        protected internal StreamReader Reader { get; }
+
+        TcpClient _client;
+        public TcpClient Client { get { return _client; } }
+        ServerObject server; // объект сервера
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+
+            _client = tcpClient;
+            server = serverObject;
+            // получаем NetworkStream для взаимодействия с сервером
+            var stream = _client.GetStream();
+            // создаем StreamReader для чтения данных
+            Reader = new StreamReader(stream);
+            // создаем StreamWriter для отправки данных
+            Writer = new StreamWriter(stream);
+        }
+
+        public async Task ProcessAsync()
+        {
+            try
+            {
+                string login = String.Empty;
+                string password = String.Empty;
+                while (!IsAuthorized)
+                {
+                    // получаем логин и пароль пользователя
+                    string? connString = await Reader.ReadLineAsync();
+                    string[] userData = connString.Split('@');
+                    try
+                    {
+                        Console.WriteLine($"Получены данные от клиента {Client.Client.RemoteEndPoint}. Данные: {connString}");
+                        _isAuthorized = await AuthenticationManager.AccessAllowed(login: userData[0], password: userData[1], database: server.Database);
+                    }
+
+                    catch (IndexOutOfRangeException)
+                    {
+
+                        await server.SinglecastMessageAsync("Ожидаются данные в формате login@password", Id);
+
+
+                    }
+
+                    if (IsAuthorized)
+                    {
+                        login = userData[0];
+                        password = userData[1];
+                        await server.SinglecastMessageAsync($"acess allowed", Id);
+
+                    }
+                    else
+                    {
+                        await server.SinglecastMessageAsync("Неверные данные.", Id);
+                        Console.WriteLine($"Введенные данные от клиента {Client.Client.RemoteEndPoint} - не подошли.");
+                    }
+
+
+                }
+
+                Console.WriteLine($"Клиент {Client.Client.RemoteEndPoint} - авторизовался под логином:{login}.");
+                string? message = null;
+
+                // в бесконечном цикле получаем сообщения от клиента
+                while (true)
+                {
+                    try
+                    {
+                        message = await Reader.ReadLineAsync();
+                        if (message == null) continue;
+                        Console.WriteLine($"Получены данные от клиента {Client.Client.RemoteEndPoint}. Данные: {message}");
+                        if (message == "spam")
+                        {
+
+                            _spamAllowed = !_spamAllowed;
+                            Console.WriteLine($"Клиент {Client.Client.RemoteEndPoint}. Рассылка = {SpamAllowed}");
+
+                        }
+
+                    }
+                    catch
+                    {
+                        message = $"{login} отключился.";
+                        Console.WriteLine(message);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // в случае выхода из цикла закрываем ресурсы
+                server.RemoveConnection(Id);
+            }
+        }
+        // закрытие подключения
+        protected internal void Close()
+        {
+            Writer.Close();
+            Reader.Close();
+            _client.Close();
+        }
+    }
+}
---
+++ Server/Models/Server/ServerObject.cs
@@ -0,0 +1,122 @@
+﻿using Models.Database;
+using System.Net.Sockets;
+using System.Net;
+using System.Xml.Linq;
+using Server.Models.Client;
+
+namespace Server.Models.Server
+{
+    class ServerObject
+    {
+        private IDatabase _database = DatabasePostgreSql.Instance;
+        public IDatabase Database { get { return _database; } }
+        TcpListener tcpListener = new TcpListener(IPAddress.Any, 8888); // сервер для прослушивания
+        List<ClientObject> clients = new List<ClientObject>(); // все подключения
+
+        public async void SpamProcessAsync()
+        {
+            Console.WriteLine("Рассылка - запущена.");
+
+            string message = "Какая-то история о Незнайке.";
+            while (true)
+            {
+                if (clients.Count != 0)
+                {
+                    Console.WriteLine("Рассылка.");
+
+                    await BroadcastMessageAsync(message);
+                    Console.WriteLine("Рассылка завершена.");
+
+                    await Task.Delay(10000);
+                }
+                else
+                {
+                    Console.WriteLine("Сообщения не отправлены -  подключений нет.");
+                    await Task.Delay(10000);
+
+                }
+            }
+        }
+
+        // прослушивание входящих подключений
+        protected internal async Task ListenAsync()
+        {
+            try
+            {
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+                    ClientObject clientObject = new ClientObject(tcpClient, this);
+                    clients.Add(clientObject);
+                    Console.WriteLine($"Новое подключение от клиента {clientObject.Client.Client.RemoteEndPoint}.");
+                    Task.Run(clientObject.ProcessAsync);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+        protected internal void RemoveConnection(string id)
+        {
+            // получаем по id закрытое подключение
+            ClientObject? client = clients.FirstOrDefault(c => c.Id == id);
+            // и удаляем его из списка подключений
+            if (client != null) clients.Remove(client);
+            client?.Close();
+        }
+
+        // трансляция сообщения подключенным клиентам
+        protected internal async Task BroadcastMessageAsync(string message, string? id = null)
+        {
+            foreach (var client in clients)
+            {
+                if (client.Id != id) // если id клиента не равно id отправителя
+                {
+                    if (client.SpamAllowed) // разрешена массовая отправка
+                    {
+
+                        await client.Writer.WriteLineAsync(message); //передача данных
+                        await client.Writer.FlushAsync();
+                        Console.WriteLine($"Клиент {client.Client.Client.RemoteEndPoint}. Отправлено сообщение {message}");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Клиент {client.Client.Client.RemoteEndPoint}. Рассылка запрещена.");
+                    }
+
+                }
+            }
+        }
+        // трансляция сообщения одному клиентам
+        protected internal async Task SinglecastMessageAsync(string message, string id)
+        {
+            foreach (var client in clients)
+            {
+                if (client.Id == id) // если id клиента равно id отправителя
+                {
+                    await client.Writer.WriteLineAsync(message); //передача данных
+                    await client.Writer.FlushAsync();
+                }
+            }
+        }
+        // отключение всех клиентов
+        protected internal void Disconnect()
+        {
+            foreach (var client in clients)
+            {
+                client.Close(); //отключение клиента
+            }
+            tcpListener.Stop(); //остановка сервера
+        }
+    }
+}
