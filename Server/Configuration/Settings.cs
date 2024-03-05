a63e1609477c660a8e33229794aff8d960b4b365
Никита Чепельский <nikitachepelskii2@gmail.com>
2024-03-05 19:35:22 +0300

    Add Config Manager

---
+++ Server/Configuration/Settings.cs
@@ -0,0 +1,28 @@
+﻿namespace Server.Configuration
+{
+    public class RootSettings
+    {
+        public DatabaseSettings Database { get; set; } = new DatabaseSettings();
+        public MessageSendingSettings MessageSending { get; set; } = new MessageSendingSettings();
+        public LoggingSettings Logging { get; set; } = new LoggingSettings();
+    }
+
+    public class DatabaseSettings
+    {
+        public string PostgreHost { get; set; } = "localhost";
+        public string PostgreDatabaseName { get; set; } = "test_database";
+        public string PostgreUsername { get; set; } = "postgres";
+        public int PostgrePassword { get; set; } = 85245613;
+        public int PostgrePort { get; set; } = 5432;
+    }
+
+    public class MessageSendingSettings
+    {
+        public int SpamTimeout { get; set; } = 5000;
+    }
+    public class LoggingSettings
+    {
+        public int LogMaxSizeInBytes { get; set; } = 20000;
+    }
+}
+    
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,29 +1,32 @@
 ﻿using Models.Logging;
 using Server.Models.Server;
-using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Server.Configuration;
+
 
 namespace Server
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
+            //IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("Configuration/appsettings.json", false, true);
+            //IConfigurationRoot root = builder.Build();
+
+            //Console.WriteLine($"Hello, {root["weather"]} world!");
+            //root["weather"] = "sdfsdsdsfsssd";
+            //Console.WriteLine($"Hello, {root["weather"]} world!");
+
+
+            RootSettings Settings = Configuration.ConfigurationManager.Instance.RootSettings;
+            
+            Console.WriteLine($"Hello, {Settings.Database.PostgrePort} world!");
+
             new LoggerInit();
             ServerObject server = new ServerObject();// создаем сервер
             Task server_task = server.ListenAsync(); // запускаем сервер
             Task.Run(server.SpamProcessAsync); // Запускаем рассылку
             await server_task;
-
-
         }
-
-
-
-
-
-
-
-
     }
-    
-}
+}   
