763ea146171b64caef9615ae4b912d2ab7c0f354
Никита Чепельский <nikitachepelskii2@gmail.com>
2024-03-06 23:04:00 +0300

    password check

--- AvaloniaClient/Views/RegWindow.axaml.cs
+++ AvaloniaClient/Views/RegWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using AvaloniaClient.Models.WindowManager;
 
@@ -5,22 +6,63 @@ namespace AvaloniaClient;
 
 public partial class RegWindow : Window
 {
-    #region constructors
+    #region Fields
+
+
+    #endregion Fields
+
+    #region Constructors
 
     public RegWindow()
     {
-        this.Closing += OnClosing;
         InitializeComponent();
+        //PasswordBox = this.FindControl<TextBox>("PasswordBox");
+        //ConfirmPasswordBox = this.FindControl<TextBox>("ConfirmPasswordBox");
+        //ValidationTextBlock = this.FindControl<TextBlock>("ValidationTextBlock");
+        // Подписываемся на событие изменения текста
+        PasswordBox.PropertyChanged += PasswordBox_PropertyChanged;
+        ConfirmPasswordBox.PropertyChanged += ConfirmPasswordBox_PropertyChanged;
+
+        this.Closing += OnClosing;
     }
 
-    #endregion constructors
+    #endregion Constructors
 
-    #region methods
+    #region Methods
 
     private async void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
     {
         WindowManager.DropRegWindow();
     }
+    private void PasswordBox_PropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        // Проверяем, что изменяемое свойство - это текст
+        if (e.Property == TextBox.TextProperty)
+        {
+            ValidatePasswords();
+        }
+    }
 
-    #endregion methods
+    private void ConfirmPasswordBox_PropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        // Проверяем, что изменяемое свойство - это текст
+        if (e.Property == TextBox.TextProperty)
+        {
+            ValidatePasswords();
+        }
+    }
+    private void ValidatePasswords()
+    {
+        if (!string.IsNullOrEmpty(PasswordBox.Text) && PasswordBox.Text == ConfirmPasswordBox.Text)
+        {
+            ValidationTextBlock.Text = ""; // Пароли совпадают
+        }
+        else
+        {
+            ValidationTextBlock.Text = "Пароли не совпадают!";
+        }
+    }
+
+
+    #endregion Methods
 }
