d75c941f876dc2476324dbca9c2648a68de200c6
Nikita Chepelskii <nikitachepelskii2@gmail.com>
2024-03-21 12:36:29 +0300

    ADD main Window lost connection logic

--- AvaloniaClient/ViewModels/MainWindowViewModel.cs
+++ AvaloniaClient/ViewModels/MainWindowViewModel.cs
@@ -5,7 +5,10 @@ using ReactiveUI;
 using Serilog;
 using System;
 using System.Net.Sockets;
+using System.Reactive;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Tmds.DBus.Protocol;
 
 namespace AvaloniaClient.ViewModels
 {
@@ -14,6 +17,13 @@ namespace AvaloniaClient.ViewModels
         #region Fields
 
         private string _message = "Wait a bit...";
+        private bool _connection;
+
+        public bool Connection
+        {
+            get { return _connection; }
+            set { this.RaiseAndSetIfChanged(ref _connection, value); }
+        }
 
         public string Message
         {
@@ -23,11 +33,21 @@ namespace AvaloniaClient.ViewModels
 
         #endregion Fields
 
+        #region Commands
+        public ReactiveCommand<Unit, Unit> SwitchToAuthWindowCommand { get; }
+
+        #endregion
+
         #region Constructors
 
         public MainWindowViewModel()
         {
             Log.Debug($"Создание главного окна.");
+            _connection = ConnectionService.Instance.Client.Connected;
+
+            ConnectionService.Instance.AddCallback(RefreshConnectionStatus);
+            SwitchToAuthWindowCommand = ReactiveCommand.Create(SwitchToAuthWindow);
+
 
             GetDataFromServerAsync();
         }
@@ -38,11 +58,8 @@ namespace AvaloniaClient.ViewModels
 
         private async Task GetDataFromServerAsync()
         {
-            Log.Information($"Получение сообщений из рассылки сервера - запущено.");
+            await ServerSpam();
 
-            ServerRequest request = new ServerRequest(command: "-spam");
-            ConnectionService.Instance.AddRequest(request);
-            ServerResponse comm_response = await ConnectionService.Instance.GetResponseAsync(response_id: request.Id, timeout: TimeSpan.FromSeconds(2));
 
             while (true)
             {
@@ -50,7 +67,7 @@ namespace AvaloniaClient.ViewModels
                 {
                     string result = await Task.Run(async () =>
                     {
-                        ServerResponse response = await ConnectionService.Instance.GetResponseAsync(response_id: 0000, timeout: TimeSpan.FromSeconds(20000));
+                        ServerResponse response = await ConnectionService.Instance.GetResponseAsync(response_id: 0000, timeout: TimeSpan.FromSeconds(20));
                         return response.Message;
                     }
                     );
@@ -63,10 +80,41 @@ namespace AvaloniaClient.ViewModels
                     WindowManager.SwitchToAuthWindow();
                     break;
                 }
+
             }
             Log.Information($"Получение сообщений из рассылки сервера - закончено.");
         }
 
         #endregion Tasks
+
+        #region Methods
+        public async void RefreshConnectionStatus()
+        {
+            Log.Debug($"Главное окно. Обновление статуса соеденения. Соеденение - {(ConnectionService.Instance.Client.Connected ? "Установлено" : "Потеряно")}");
+
+            //Connection = ConnectionService.Instance.Client.Connected;
+            Connection = false;
+
+        }
+        public async Task ServerSpam()
+        {
+            Log.Information($"Получение сообщений из рассылки сервера - запущено.");
+
+            ServerRequest request = new ServerRequest(command: "-spam");
+            ConnectionService.Instance.AddRequest(request);
+            ServerResponse comm_response = await ConnectionService.Instance.GetResponseAsync(response_id: request.Id, timeout: TimeSpan.FromSeconds(2));
+        }
+
+        #endregion Methods
+
+        #region CommMethods
+
+        public async void SwitchToAuthWindow()
+        {
+            Log.Debug($"Главное окно. Кнопка возврата к авторизации нажата.");
+            WindowManager.SwitchToAuthWindow();
+
+        }
+        #endregion
     }
 }
